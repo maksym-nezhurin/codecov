089baadd02c8a3a1d694e5c98055782aa0eaa5e6
Maksym Nezhurin <maksym.nezhurin@wework.com>
2022-04-07 15:26:06 +0300

    init commit

---
+++ src/components/Link/Link.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Link from './Link';
+
+test('Link changes the class when hovered', () => {
+    const component = renderer.create(
+        <Link page="http://www.google.com">Google</Link>,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // manually trigger the callback
+    tree.props.onMouseEnter();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // manually trigger the callback
+    tree.props.onMouseLeave();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Should generate link with # if the property page is not passed', () => {
+    const component = renderer.create(
+        <Link>Form</Link>,
+    );
+    let tree = component.toJSON();
+
+    expect(tree.props.href).toEqual('#')
+});
+
+test('new test casenew link for ', () => {
+    const component = renderer.create(
+        <Link isNew={true}>New link</Link>,
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+})
