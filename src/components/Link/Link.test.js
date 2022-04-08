8b25b9a175d5e770687d98b855f5cad828a0ad43
Maksym Nezhurin <maksym.nezhurin@wework.com>
2022-04-08 16:21:46 +0300

    reduce code coverage

--- src/components/Link/Link.test.js
+++ src/components/Link/Link.test.js
@@ -2,26 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Link from './Link';
 
-test('Link changes the class when hovered', () => {
-    const component = renderer.create(
-        <Link page="http://www.google.com">Google</Link>,
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-
-    // manually trigger the callback
-    tree.props.onMouseEnter();
-    // re-rendering
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-
-    // manually trigger the callback
-    tree.props.onMouseLeave();
-    // re-rendering
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-});
-
 test('Should generate link with # if the property page is not passed', () => {
     const component = renderer.create(
         <Link>Form</Link>,
